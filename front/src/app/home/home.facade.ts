1ce45ca776d376d65b4027c3beebd2415dc4f92d
jdaniielc <jdsc2@cin.ufpe.br>
2024-07-17 17:10:31 -0300

    Chore: Prepare to change architecture folders

--- front/src/app/home/home.facade.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { HomeState } from './state/home.state';
-
-@Injectable()
-export class HomeFacade {
-  constructor(
-    readonly state: HomeState
-  ) {}
-}
