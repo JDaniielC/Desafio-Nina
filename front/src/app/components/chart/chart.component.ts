606c07f5d3f678034c5b57401b274e2b1f4577e3
jdaniielc <jdsc2@cin.ufpe.br>
2024-07-18 17:06:18 -0300

    Feat: Create chart component

---
+++ front/src/app/components/chart/chart.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { ChartOptions } from '../../types/apexchart';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-chart',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgApexchartsModule
+  ],
+  templateUrl: './chart.component.html',
+  styleUrl: './chart.component.scss'
+})
+export class ChartComponent implements OnInit {
+  @Input() chartOptions!: Observable<ChartOptions>;
+  options: ChartOptions = {} as ChartOptions;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.chartOptions.subscribe(res => {
+      this.options = res;
+    });
+  }
+
+}
