a37dd9b32edd1afd659d7f949d2da05a95b7929b
jdaniielc <jdsc2@cin.ufpe.br>
2024-07-21 08:12:40 -0300

    feat: Update app configuration to include HTTP interceptor and loading component

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,22 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig, provideZoneChangeDetection
+} from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { homeInitializerProvider } from './app.initializer';
 import { Facade } from './app.facade';
 import { State } from './state/state';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { ComplaintsService } from './services/complaints.service';
+import { InterceptorProvider } from './core/http-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
     Facade,
     State,
     ComplaintsService,
-    homeInitializerProvider
+    InterceptorProvider,
+    homeInitializerProvider,
   ],
 };
