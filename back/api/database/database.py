5f2c30335a6571d9dfa244a83c61591fda469dc6
jdaniielc <jdsc2@cin.ufpe.br>
2024-07-22 09:03:02 -0300

    feat: update database to use sqlalchemy and remove unused Schema

--- back/api/database/database.py
+++ back/api/database/database.py
@@ -1,31 +1,21 @@
 from .db_exceptions import MonthNumberException
+from sqlalchemy import create_engine
+from sqlalchemy.sql import func
+from sqlalchemy.orm import Session
+from settings import Settings
+from models import User, Complaint
 from datetime import datetime
-import json
-import os
 
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-USERS_FILE_PATH = os.path.join(__location__, "mocked_users_data.json")
-COMPLAINTS_FILE_PATH = os.path.join(__location__, "mocked_complaints_data.json")
-DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
+DATE_FORMAT = "%Y-%m-%d"
+engine = create_engine(Settings().DATABASE_URL) 
 
 class Database:
-    def __init__(self) -> None:
-        self.users = []
-        with open(USERS_FILE_PATH, 'r') as file:
-            self.users = json.load(file)
-        
-        self.complaints = []
-        with open(COMPLAINTS_FILE_PATH, 'r') as file:
-            self.complaints = json.load(file)
-
-    def _increment_count(self, d: dict, key: str):
+    def __increment_count(self, d: dict, key: str):
         if key not in d:
             d[key] = 0
         d[key] += 1
 
-    def _get_age(self, birthdate: datetime):
+    def __get_age(self, birthdate: datetime):
         """ Returns an age in years based on the birthdate and the current date. """
         today = datetime.today()
         age = today.year - birthdate.year
@@ -33,9 +23,9 @@ class Database:
             age -= 1
         return age
 
-    def _get_age_group(self, birthdate: datetime):
+    def __get_age_group(self, birthdate: datetime):
         """ Returns a string representing an age_group based on the birthdate. """
-        age = self._get_age(birthdate)
+        age = self.__get_age(birthdate)
         
         if age < 14:
             return "< 14"
@@ -51,11 +41,7 @@ class Database:
             return "50 - 59"
         return "> 60"
     
-    def _get_date_elements(self, d: datetime):
-        """ Returns day, month and year from a datetime object as int values. """
-        return d.day, d.month, d.year
-    
-    def _translate_month_int_to_name(self, month_num: int):
+    def __translate_month_int_to_name(self, month_num: int):
         months = {
             1: "Jan",
             2: "Fev",
@@ -78,63 +64,124 @@ class Database:
 
         return month_name
 
-    def _get_user_data_to_complaint(self, complaint: dict):
-        user_id = complaint['user_id']
-        user = self.get_user(user_id)
-        for key, value in user.items(): 
-            complaint[f'user_{key}'] = value
-        return complaint
-
-
-    def insert_complaint(self):
-        pass
-
-    def get_complaints(self, start_date: str = None, end_date: str = None):
+    def __get_all_complaints(self):
+        with Session(engine) as session:
+            return session.query(Complaint).all()
+    
+    def __complaint_user_treatment(self, complaints: list):
         complaints_with_user_data = []
-        
-        for complaint in self.complaints:
-            complaint = self._get_user_data_to_complaint(complaint)
-
-            complaint_date = datetime.strptime(complaint['date'], DATE_FORMAT)
-            if (start_date is not None and start_date > complaint_date
-                or end_date is not None and end_date < complaint_date):
-                continue
+        for complaint, user in complaints:
+            complaint = complaint.__dict__
+            user = user.__dict__
+            for key, value in user.items():
+                complaint[f'user_{key}'] = value
             complaints_with_user_data.append(complaint)
 
         return complaints_with_user_data
 
-    def get_complaint(self, _id: str = None, user_id: str = None):
-        if _id is not None:
-            result = list(filter(lambda x: x['id'] == _id, self.complaints))
-            if (len(result) > 0):
-                return self._get_user_data_to_complaint(result[0])
+    def get_complaints_filter(self, start_date: str = None, end_date: str = None):
+        filters = list()
+        if start_date is not None:
+            filters.append(Complaint.date >= start_date)
+        if end_date is not None:
+            filters.append(Complaint.date <= end_date)
 
-        if user_id is not None:
-            result = list(filter(lambda x: x['user_id'] == user_id, self.complaints))
-            if (len(result) > 0):
-                return result
-        
-        return None
+        return filters
     
+    def get_complaints(self, start_date: str = None, end_date: str = None):
+        with Session(engine) as session:
+            filters = self.get_complaints_filter(start_date, end_date)
+            complaints = (session
+                .query(Complaint, User)
+                .join(User, Complaint.user_id == User.id)
+                .filter(*filters)
+                .order_by(Complaint.id)
+                .all())
+            return self.__complaint_user_treatment(complaints)
+        
+    def get_complaints_with_pagination(self, start_date: str = None,
+                                       end_date: str = None, page: int = 1,
+                                       limit: int = 6):
+        offset = (page - 1) * limit
+        with Session(engine) as session:
+            filters = self.get_complaints_filter(start_date, end_date)
+            complaints = (session
+                .query(Complaint, User)
+                .join(User, Complaint.user_id == User.id)
+                .filter(*filters)
+                .order_by(Complaint.id)
+                .offset(offset)
+                .limit(limit)
+                .all())
+            total_complaints = (session
+                .query(Complaint)
+                .filter(*filters)
+                .count())
+            total_pages = (total_complaints / limit) + 1
+            complaints = self.__complaint_user_treatment(complaints)
+            return complaints, total_pages
+        
+    def get_complaint(self, _id: str = None):
+        with Session(engine) as session:
+            complaints = (session
+                         .query(Complaint, User)
+                         .join(User, Complaint.user_id == User.id)
+                         .filter(Complaint.id == _id)
+                         .all())
+            return self.__complaint_user_treatment(complaints)[0]
+    
+    def get_complaints_from_user(self, user_id: str):
+        with Session(engine) as session:
+            complaints = (session
+                         .query(Complaint, User)
+                         .join(User, Complaint.user_id == User.id)
+                         .filter(User.id == user_id)
+                         .all())
+            return self.__complaint_user_treatment(complaints)
+    
+    def insert_complaint(self, new_complaint: dict):
+        with Session(engine) as session:
+            complaint = Complaint(**new_complaint)
+            session.add(complaint)
+            session.commit()
+
+    def update_complaint(self, _id: str, new_complaint: dict):
+        with Session(engine) as session:
+            (session
+                .query(Complaint)
+                .filter(Complaint.id == _id)
+                .update(new_complaint))
+            session.commit()
+
+    def delete_complaint(self, _id: str):
+        with Session(engine) as session:
+            session.query(Complaint).filter(Complaint.id == _id).delete()
+            session.commit()
+
+    def group_by_gender(self):
+        with Session(engine) as session:
+            tuples = (session
+                    .query(User.gender, func.count(User.gender))
+                    .select_from(Complaint)
+                    .join(User, Complaint.user_id == User.id)
+                    .group_by(User.gender))
+            return {key: value for key, value in tuples} 
+
     def group_by(self, complaint_key: str):
         """ Groups the number of complaints by each value of given key. """
-        grouped_data = {}
-        complaints = self.get_complaints()
-        for complaint in complaints:
-            value = complaint[complaint_key]
-            self._increment_count(grouped_data, value)
-
-        return grouped_data
-    
+        column = getattr(Complaint, complaint_key)
+        with Session(engine) as session:
+            tuples = session.query(column, func.count(column)).group_by(column).all()
+            return {key: value for key, value in tuples}
+   
     def group_by_month(self):
         """ Groups the number of complaints by each month. """
         grouped_data = {}
-        complaints = self.get_complaints()
+        complaints = self.__get_all_complaints()
         for complaint in complaints:
-            date = datetime.strptime(complaint['date'], DATE_FORMAT)
-            _, month, _ = self._get_date_elements(date)
-            month_name = self._translate_month_int_to_name(month)
-            self._increment_count(grouped_data, month_name)
+            date = datetime.strptime(complaint.date, DATE_FORMAT)
+            month_name = self.__translate_month_int_to_name(date.month)
+            self.__increment_count(grouped_data, month_name)
 
         return grouped_data
     
@@ -144,24 +191,17 @@ class Database:
         complaints = self.get_complaints()
         for complaint in complaints:
             user_birthdate = datetime.strptime(complaint['user_birthdate'], DATE_FORMAT)  
-            age_group = self._get_age_group(user_birthdate) 
-            self._increment_count(grouped_data, age_group)
+            age_group = self.__get_age_group(user_birthdate) 
+            self.__increment_count(grouped_data, age_group)
 
         return grouped_data
-    
-    def update_complaint(self):
-        pass
-
-    def delete_complaint(self):
-        pass
 
     def get_users(self):
-        return self.users
+        with Session(engine) as session:
+            return session.query(User).all()
 
     def get_user(self, _id: str = None):
-        result = list(filter(lambda x: x['id'] == _id, self.users))
-        if len(result) > 0:
-            return result[0]
-        return None
+        with Session(engine) as session:
+            return session.query(User).filter(User.id == _id).first()
     
 client = Database()
